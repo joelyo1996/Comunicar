e0135b79ba1fef515a93bed7fcf04396fc19c944
joelyo1996 <38585894+joelyo1996@users.noreply.github.com>
2020-04-06 19:39:01 -0300

    nuevo

---
+++ global.gd
@@ -0,0 +1,17 @@
+extends Node
+var entrada_comedor= 0
+var contadordialogo = 0
+var entrada_mapa = 0
+var randon_position = 0
+var newEnemigo
+var colicion_cajas 
+var con
+var tornillo
+var entradasFerreteria = 0
+var entradaspizzaron = 0
+var entradasEscuela = 0
+var sorbete = 0
+var cajas = false
+var silencio = false
+func _ready():
+	pass
